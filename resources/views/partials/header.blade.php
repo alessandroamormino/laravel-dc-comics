2931598dc7791b3cc87c5011a3f74826f5ad0801
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-16 14:24:13 +0200

    fix: removed link in nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,8 +7,8 @@
       <div class="nav-center">
         <ul>
           @foreach($links as $link)
-              {{-- <li><a href="#">{{$link}}</a></li> --}}
-              <li><a href="{{ $link == 'comics' ? '/'.$link : '#' }}">{{ $link }}</a></li>
+              <li><a href="#">{{$link}}</a></li>
+              {{-- <li><a href="{{ $link == 'comics' ? '/'.$link : '#' }}">{{ $link }}</a></li> --}}
           @endforeach
         </ul>
       </div>
